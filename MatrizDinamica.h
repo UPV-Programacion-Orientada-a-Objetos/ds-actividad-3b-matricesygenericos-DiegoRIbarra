3a4c6a103c4a737afa7b364818b91f647221285a
2430020 <2430020@upv.edu.mx>
2025-10-16 09:00:29 -0600

    Actividad 3B terminada

--- MatrizDinamica.h
+++ MatrizDinamica.h
@@ -7,4 +7,103 @@ private:
 
 public:
     // Constructor, Destructor, Métodos de gestión y operaciones
+    // Constructor
+    MatrizDinamica(int filas, int columnas){
+        this->filas = filas;
+        this->columnas = columnas;
+        datos = new T*[filas];
+        for(int i = 0; i < filas; i++) {
+            datos[i] = new T[columnas];
+        }
+    }
+    //Metodos
+    void llenarMatriz(){
+        for(int i = 0; i < filas; i++) {
+            for(int j = 0; j < columnas; j++) {
+                std::cout << "Ingrese el valor para la posición [" << i << "][" << j << "]: ";
+                std::cin >> datos[i][j];
+            }
+        }
+    }
+    void redimensionarMatriz(int newfls, int newcolmns){
+        T **aux= new T*[newfls];//creamos la matriz auxiliar
+        for(int i = 0; i < newfls; i++) {
+            aux[i] = new T[newcolmns];//creamos las columnas de la matriz auxiliar
+            for(int j = 0; j < newcolmns; j++) {
+                aux[i][j] = 0;//inicializamos la matriz auxiliar en 0
+            }    
+        }
+        //obtenemos la cantiddad de filas y columnas a copiar
+        int minfls= (filas< newfls)?filas:newfls;
+        int mincols= (columnas< newcolmns)?columnas:newcolmns;
+        //ya creada le pasamos los datos de la matriz original a la auxiliar
+        for(int i = 0; i < minfls; i++) {
+            for(int j = 0; j < mincols; j++) {
+                aux[i][j] = datos[i][j];
+            }
+        }
+        //liberamos la memoria de la matriz original
+        for(int i = 0; i < filas; i++) {
+            delete[] datos[i];
+        }
+        delete[] datos;
+        datos=aux;//apuntamos datos a la matriz auxiliar
+        filas=newfls;//actualizamos filas y columnas
+        columnas=newcolmns;
+    }
+    void imprimirMatriz(){
+        for(int i = 0; i < filas; i++) {
+            for(int j = 0; j < columnas; j++) {
+                std::cout << datos[i][j] << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    int getFilas(){
+        return filas;
+    }
+    int getColumnas(){
+        return columnas;
+    }
+    
+    template <typename U>
+
+    static MatrizDinamica<U> conversion(MatrizDinamica<T> &A){
+        MatrizDinamica<U> Z(A.getFilas(), A.getColumnas());//se crea matriz auxiliar
+        for(int i = 0; i < A.getFilas(); i++) {
+            for(int j = 0; j < A.getColumnas(); j++) {
+                Z.datos[i][j] = static_cast<U>(A.datos[i][j]);//static_cast hace conversion automatica de T a U
+            }
+        }
+        return Z;
+    }
+
+
+
+    static MatrizDinamica<T> multiplicar(MatrizDinamica<T> &A, MatrizDinamica<U> &B){
+        bool verificacion = (A.getColumnas() == B.getFilas()) ? true : false;
+        if (verificacion){
+            //como las dimensiones son compatibles se puede multiplicar
+            MatrizDinamica<T> C(A.getFilas(), B.getColumnas());
+            for(int i = 0; i < A.getFilas(); i++) {
+                for(int j = 0; j < B.getColumnas(); j++) {
+                    C.datos[i][j] = 0; // Inicializar el elemento con el fin de evitar valores basura
+                    for(int k = 0; k < A.getColumnas(); k++) {
+                        C.datos[i][j] += A.datos[i][k] * B.datos[k][j];
+                    }
+                }
+            }
+            return C;
+        }else{
+            std::cout<<" No se puede realizar la multiplicacion de las matrices A y B por incompatibilidad de dimensiones."<<"\n";
+            return MatrizDinamica<T>(0, 0); // Devuelve una matriz de 0x0
+        }
+    }
+    ~MatrizDinamica(){
+        for(int i = 0; i < filas; i++) {
+            delete[] datos[i];
+        }
+        delete[] datos;
+    }
 };
--- main.cpp
+++ main.cpp
@@ -2,5 +2,46 @@
 #include "MatrizDinamica.h"
 
 int main() {
+    int fls, colmns;
+    //matriz A
+    std::cout<<" Prueba de Matriz ENTERA (Matriz A)"<<"\n";
+    std::cout<<" Ingrese el numero de filas: ";
+    std::cin>>fls;
+    std::cout<<" Ingrese el numero de columnas: ";
+    std::cin>>colmns;
+    std::cout<<" Creando Matriz (Tipo INT) A de "<<fls<<" filas y "<<colmns<<" columnas."<<"\n";
+    MatrizDinamica<int> A(fls, colmns);
+    std::cout<<" Llenando Matriz (Tipo INT) A con valores."<<"\n";
+    A.llenarMatriz();
+
+    //redimensionado de la matriz A
+    std::cout<<" Redimensionando Matriz (Tipo INT) A."<<"\n";
+    std::cout<<" Ingrese el nuevo numero de filas: ";
+    std::cin>>fls;
+    std::cout<<" Ingrese el nuevo numero de columnas: ";
+    std::cin>>colmns;
+    A.redimensionarMatriz(fls, colmns);
+    std::cout<<" Matriz (Tipo INT) A redimensionada a "<<fls<<" filas y "<<colmns<<" columnas."<<"\n";
+    A.imprimirMatriz();
+
+    //matriz B
+    std::cout<<" Prueba de Matriz Float (Matriz B)"<<"\n";
+    std::cout<<" Ingrese el numero de filas: ";
+    std::cin>>fls;
+    std::cout<<" Ingrese el numero de columnas: ";
+    std::cin>>colmns;
+    std::cout<<" Creando Matriz (Tipo FLOAT) B de "<<fls<<" filas y "<<colmns<<" columnas."<<"\n";
+    MatrizDinamica<float> B(fls, colmns);
+    std::cout<<" Llenando Matriz (Tipo FLOAT) B con valores."<<"\n";
+    B.llenarMatriz();
+
+    //conversion de la matriz A a float
+    MatrizDinamica<float> A_float = MatrizDinamica<int>::conversion<float>(A);
+    //multiplicacion y creacion de la matriz C
+    std::cout<<" Multiplicando Matriz (Tipo INT) A y B para crear Matriz C."<<"\n";
+    MatrizDinamica<float> C = MatrizDinamica<float>::multiplicar(A_float, B);
+    std::cout<<" Matriz (Tipo FLOAT) C resultante de la multiplicacion:"<<"\n";
+    C.imprimirMatriz();
+
     return 0;
 }
